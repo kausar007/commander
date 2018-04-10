4c202dfb76a24f275c33c917054c4a211d804f65
Kausar Mehmood <kausar.bangash@gmail.com>
2018-04-10 22:03:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+commander
+===========
+
+A tool to search for your commands from terminal. It uses a simple text file as command store and uses regex to match commands based on your search query.
+
+# Installation
+pip install commander
+
+# Usage
+```
+commander docker
+commander docker st
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'commander',
+    packages = ['commander'], 
+    version = '0.0.1',
+    description = 'Search for your command from terminal',
+    author = 'Kausar Mehmood',
+    author_email = 'kausar.bangash@gmail.com',
+    url = 'https://github.com/kausar007/commander',
+    download_url = 'https://github.com/kausar007/commander/tarball/0.0.1',
+    keywords = ['command', 'search', 'command line', "find", "console"], 
+    classifiers = [],  
+)
